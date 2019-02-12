bd0e8d388eff11409108aad67d83519c8e37256a
deepak <deepak.dhamija@evry.com>
2019-02-12 15:52:24 +0530

    Display Clients

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Stateful } from "./components/Stateful/Stateful";
+import { Clients } from "./components/Clients/Client";
 declare let module: any;
 
-ReactDOM.render(<Stateful />,
-document.getElementById("root"));
+ReactDOM.render(<Clients />,
+    document.getElementById("root"));
 
 if (module.hot) {
     module.hot.accept();
- }
+}
