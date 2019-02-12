eb87ebfc8ab6c0b6a300b50eb58453ff1930e872
jagdishkumar17 <jagdish.kumar@spanservices.com>
2019-02-12 15:56:41 +0530

    Update app.ts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { RefsAndDOM } from "./components/ReactJSComponentAPI/RefsAndDOM";
+import { ForceUpdate } from "./components/ReactJSComponentAPI/ForceUpdate";
 declare let module: any;
 
-ReactDOM.render(<RefsAndDOM />,
+ReactDOM.render(<ForceUpdate />,
 document.getElementById("root"));
 
 if (module.hot) {
